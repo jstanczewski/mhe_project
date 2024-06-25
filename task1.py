13cd3e6f1546adb68f2f4bbc69cc70fb2bffb2ea
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2024-06-25 22:20:19 +0200

    Added neighbor solution

--- task1.py
+++ task1.py
@@ -16,16 +16,74 @@ def subset_sum(arr, target_sum):
             else:
                 dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
 
-    return dp[arr_len][target_sum]
+    # Odtworzenie podzbioru, który tworzy tę sumę
+    result = []
+    i, j = arr_len, target_sum
+    while i > 0 and j > 0:
+        if dp[i - 1][j]:
+            i -= 1
+        else:
+            result.append(arr[i - 1])
+            j -= arr[i - 1]
+            i -= 1
+    is_sum_possible = dp[arr_len][target_sum]
+    return is_sum_possible, result
 
 
-# Przykładowe użycie:
-# arr = [3, 4, 5]
-# T = 9
+def closest_subset_sum(arr, target_sum):
+    arr_len = len(arr)
 
+    dp = [[False] * (target_sum + 1) for _ in range(arr_len + 1)]
+    closest = [0] * (target_sum + 1)
+    print(closest)
+
+    for i in range(arr_len + 1):
+        dp[i][0] = True
+
+    for i in range(1, arr_len + 1):
+        for j in range(1, target_sum + 1):
+            if j < arr[i - 1]:
+                dp[i][j] = dp[i - 1][j]
+                if dp[i][j]:
+                    closest[j] = max(closest[j], j)
+            else:
+                dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
+                if dp[i][j]:
+                    closest[j] = max(closest[j], j)
+    print(closest)
+
+    # znalezienie najbliższej sumy
+    best_sum = 0
+    for j in range(target_sum + 1):
+        if dp[arr_len][j]:
+            best_sum = max(best_sum, closest[j])
+
+    result = []
+    i, j = arr_len, best_sum
+    while i > 0 and j > 0:
+        if dp[i - 1][j]:
+            i -= 1
+        else:
+            result.append(arr[i - 1])
+            j -= arr[i - 1]
+            i -= 1
+
+    return best_sum, result
+
+# przykładowe użycie:
+# arr = [3, 4, 6, 10]
+# target_sum = 12
+
+# z inputu
 input_elements = input("List of elements, separated by space: ")
 arr = input_elements.split()
 arr = [int(item) for item in arr]
 target_sum = int(input("Target: "))
 
-print(subset_sum(arr, target_sum))
+is_sum_possible, result = subset_sum(arr, target_sum)
+print(f"Czy suma {target_sum} jest osiągalna: {is_sum_possible}")
+if is_sum_possible:
+    print(f"Elementy: {result}")
+else:
+    best_sum, elements = closest_subset_sum(arr, target_sum)
+    print(f"Najbliższa suma: {best_sum}, Elementy tworzące tę sumę: {elements}")
